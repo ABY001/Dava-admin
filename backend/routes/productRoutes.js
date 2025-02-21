7f2de43fa236b9142602fe09f8d37ec170b0c7ff
Abiola Fadipe <abiolafadipe88@gmail.com>
2025-02-21 11:47:59 +0100

    removed auth on products route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getProducts);
+router.get("/", getProducts);
 router.get("/:id", authMiddleware, getProduct);
 router.post("/", authMiddleware, createProduct);
 router.put("/:id", authMiddleware, updateProduct);
